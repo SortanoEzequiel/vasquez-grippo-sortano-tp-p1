af91ce8a3c8991948590d901d0a122db435f0ea7
EzequielSortano <sortano59@gmail.com>
2024-10-21 20:48:27 -0300

    Update Juego.java

--- ProyectoLimpio/src/juego/Juego.java
+++ ProyectoLimpio/src/juego/Juego.java
@@ -1,6 +1,6 @@
 package juego;
 
-
+import java.util.concurrent.TimeUnit;
 import java.awt.Color;
 import java.awt.Image;
 import java.util.Random;
@@ -17,24 +17,31 @@ public class Juego extends InterfaceJuego
 	// Variables y métodos propios de cada grupo
 	// ...
 	private Image fondo = Herramientas.cargarImagen("imagenes/fondogame.jpg");
-	private Pisos[] pisos; // creamos un vector de pisos
+	private Gnomo[] gnomos;
+	private Bloques bl;
+
 	
 	Juego()
 	{
 		Random rand = new Random();
 		// Inicializa el objeto entorno
 		this.entorno = new Entorno(this, "Proyecto para TP", 800, 600);
-		iniciarJuego();
+		this.bl = new Bloques();
+	
+		this.gnomos= new Gnomo[4];
+		
+		for(int i = 0; i <this.gnomos.length; i++) {
+			gnomos[i] = new Gnomo(400, 118,30, 1);
+		}
+		
+		
+		
+	
 		// Inicia el juego!
 		this.entorno.iniciar();
 	}
-	 private void iniciarJuego(){
-		 int cantPisos = 5 ;  // genero 5 pisos por defecto... se pueden modificar. 
-		 this.pisos = new Pisos[cantPisos];
-		 for (int i = 0; i < 5; i++) {
-	            pisos[i] = new Pisos(600 - i * 150, 5, 80, 20); // Crear pisos con bloques cada uno
-	        }
-	 }
+	
+	
 	/**
 	 * Durante el juego, el método tick() será ejecutado en cada instante y 
 	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
@@ -46,35 +53,119 @@ public class Juego extends InterfaceJuego
 		// ...
 		fondo = Herramientas.cargarImagen("imagenes/skyhill.png"); // cargamos la imagen de fondo
 		entorno.dibujarImagen(fondo, entorno.ancho() / 2, entorno.alto() / 2, 0, 2.5); // la dibujamos.
-
+		
+		bl.dibujarBloques(entorno);
+		
+		Bloque[] bloq = bl.getTotalBloques();
+		
 		Image casita = Herramientas.cargarImagen("imagenes/house.png");
-		entorno.dibujarImagen(casita, 400, 68, 0, 0.1);
+		entorno.dibujarImagen(casita, 400, 106, 0, 0.1);
+	   
+			for(int i = 0 ; i < gnomos.length; i++) {
+				gnomos[i].dibujar(entorno);
+				if(gnomos[i].getY() == 118) gnomos[i].moverIzquierda();
+				
+				if(bordeBloque(gnomos[i],bloq[9])) gnomos[i].caer();
+				
+				if(colisionBloqueGnomo(gnomos[i], bloq[7])) {
+					gnomos[i].moverIzquierda();
+				}
+				if(bordeBloque(gnomos[i],bloq[7]) && gnomos[i].getY() + gnomos[i].getRadio()  >= 275 && gnomos[i].getY() + gnomos[i].getRadio()  < 400) gnomos[i].caer();
+				
+				if(colisionBloqueGnomo(gnomos[i], bloq[8])) {
+					gnomos[i].moverIzquierda();
+				}
+				if(bordeBloque(gnomos[i],bloq[8]) && gnomos[i].getY() + gnomos[i].getRadio()  >= 275 && gnomos[i].getY() + gnomos[i].getRadio()  < 400) gnomos[i].caer();
+				
+				if(colisionBloqueGnomo(gnomos[i], bloq[5])) {
+					gnomos[i].moverIzquierda();
+				}
+				if(bordeBloque(gnomos[i],bloq[5]) && gnomos[i].getY() + gnomos[i].getRadio()  >= 400 && gnomos[i].getY() + gnomos[i].getRadio()  < 525) gnomos[i].caer();
+				
+				if(colisionBloqueGnomo(gnomos[i], bloq[4])) {
+					gnomos[i].moverIzquierda();
+				}
+				if(bordeBloque(gnomos[i],bloq[4]) && gnomos[i].getY() + gnomos[i].getRadio()  >= 400 && gnomos[i].getY() + gnomos[i].getRadio()  < 525) gnomos[i].caer();
+				
+				if(colisionBloqueGnomo(gnomos[i], bloq[6])) {
+					gnomos[i].moverDerecha();
+				}
+				if(bordeBloque(gnomos[i],bloq[6]) && gnomos[i].getY() + gnomos[i].getRadio()  >= 400 && gnomos[i].getY() + gnomos[i].getRadio()  < 525) gnomos[i].caer();
+				
+				if(colisionBloqueGnomo(gnomos[i], bloq[3])) {
+					gnomos[i].moverDerecha();
+				}
+				if(bordeBloque(gnomos[i],bloq[3]) && gnomos[i].getY() + gnomos[i].getRadio()  >= 525) gnomos[i].caer();
+				
+				if(colisionBloqueGnomo(gnomos[i], bloq[2])) {
+					gnomos[i].moverIzquierda();
+				}
+				if(bordeBloque(gnomos[i],bloq[2]) && gnomos[i].getY() + gnomos[i].getRadio()  >= 525) gnomos[i].caer();
+				
+				if(colisionBloqueGnomo(gnomos[i], bloq[1])) {
+					gnomos[i].moverIzquierda();
+				}
+				if(bordeBloque(gnomos[i],bloq[1]) && gnomos[i].getY() + gnomos[i].getRadio()  >= 525) gnomos[i].caer();
+				
+				if(colisionBloqueGnomo(gnomos[i], bloq[0])) {
+					gnomos[i].moverIzquierda();
+				}
+				if(bordeBloque(gnomos[i],bloq[0]) && gnomos[i].getY() + gnomos[i].getRadio()  >= 525) gnomos[i].caer();
+				
 
-		actualizarJuego();
+			
+				
+				
+				
+				
+			
+				
+					
+				
+					
+						
+					
+					 
+	    
+			   System.out.print(gnomos[i].getY() + gnomos[i].getRadio());
+		     }
+		
+		
+		
+		
+		
+			
+		
 
 	}
 
-	private void actualizarJuego() {
-
-		Image bloque = Herramientas.cargarImagen("imagenes/ground.png");
-		entorno.dibujarImagen(bloque, 400, 110, 0, 0.5);
-		entorno.dibujarImagen(bloque, 300, 200, 0, 0.5);
-		entorno.dibujarImagen(bloque, 500, 200, 0, 0.5);
-		entorno.dibujarImagen(bloque, 600, 300, 0, 0.5);
-		entorno.dibujarImagen(bloque, 400, 300, 0, 0.5);
-		entorno.dibujarImagen(bloque, 200, 300, 0, 0.5);
-		entorno.dibujarImagen(bloque, 100, 400, 0, 0.5);
-		entorno.dibujarImagen(bloque, 300, 400, 0, 0.5);
-		entorno.dibujarImagen(bloque, 500, 400, 0, 0.5);
-		entorno.dibujarImagen(bloque, 700, 400, 0, 0.5);
-		entorno.dibujarImagen(bloque, 0, 500, 0, 0.5);
-		entorno.dibujarImagen(bloque, 200, 500, 0, 0.5);
-		entorno.dibujarImagen(bloque, 400, 500, 0, 0.5);
-		entorno.dibujarImagen(bloque, 600, 500, 0, 0.5);
-		entorno.dibujarImagen(bloque, 800, 500, 0, 0.5);
-
+	
+	private boolean colisionBloqueGnomo(Gnomo gnomos, Bloque bloq)
+	{
+		boolean colisionX = bloq.getXizq()  < gnomos.getX() &&
+				            bloq.getXder()  > gnomos.getX();
+							
+		boolean colisionY = gnomos.getY() + gnomos.getRadio() == bloq.getY();
+							
+		return colisionX && colisionY;
+	}
+    
+	private boolean bordeBloque(Gnomo gnomos,Bloque bloq) {
+		
+		boolean bloque = gnomos.getX() == bloq.getXizq() || gnomos.getX() == bloq.getXder();
+		return bloque ;
 	}
 	
+private void moverGnomo(Gnomo gnomos) {
+	    Random random = new Random();
+	    int randomInt = random.nextInt(1);
+	    System.out.println(randomInt);
+		if(randomInt == 0) {
+			gnomos.moverDerecha();
+		}
+		gnomos.moverIzquierda();
+	}
+
 	
 	@SuppressWarnings("unused")
 	public static void main(String[] args)

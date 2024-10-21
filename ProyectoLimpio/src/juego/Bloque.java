044349229a10e1c18a4218cb35f362155317d948
EzequielSortano <sortano59@gmail.com>
2024-10-21 20:51:37 -0300

    Create Bloque.java

---
+++ ProyectoLimpio/src/juego/Bloque.java
@@ -0,0 +1,35 @@
+package juego;
+
+import java.awt.Image;
+
+public class Bloque {
+	private int x;
+	private int y;
+	private Image img;
+	
+	Bloque(Image imagen,int x,int y){
+		this.x=x;
+		this.y=y;
+		this.img=imagen;				
+	}
+	
+	public int getX() {
+        return x;
+    }
+	
+	public int getXder() {
+        return x+85;
+    }
+	
+	public int getXizq() {
+        return x-85;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Image getImg() {
+        return img;
+    }
+}

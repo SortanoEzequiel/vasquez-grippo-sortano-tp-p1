ca08451b7ef6f37b75b711dda0dfe0dfe13a3993
Kevin Grippo <grippokevin@gmail.com>
2024-11-12 14:46:50 -0300

    tortugas caen en el ultimo escalon, se saca el doble salto y se agrega salto dependiendo del bloque

--- ProyectoLimpio/src/juego/Gnomo.java
+++ ProyectoLimpio/src/juego/Gnomo.java
@@ -1,9 +1,7 @@
 package juego;
 
-import java.awt.Color;
 import java.awt.Image;
 import java.util.Random;
-
 import entorno.Entorno;
 import entorno.Herramientas;
 
@@ -15,78 +13,65 @@ public class Gnomo {
 	public boolean direccion;
 	public boolean seCayo;
 	public boolean esActivo;
-	
-	public Gnomo() {	
+
+	public Gnomo() {
 		this.x = 400;
-		this.y = 130-17;
+		this.y = 130 - 17;
 		this.radio = 30;
 		this.velocidad = 2;
-		this.direccion=inicioRandom();
-		this.seCayo=false;
-		this.esActivo=false;
+		this.direccion = inicioRandom();
+		this.seCayo = false;
+		this.esActivo = false;
 	}
-	
-	public void asignar(Gnomo[] gnomos){
-		for(int i = 0; i <gnomos.length; i++) {
+
+	public void asignar(Gnomo[] gnomos) {
+		for (int i = 0; i < gnomos.length; i++) {
 			gnomos[i] = new Gnomo();
 		}
 		gnomos[0].activar();
 	}
 
-	public void caer()
-	{
+	public void caer() {
 		this.y = this.y + this.velocidad;
 	}
-	
+
 	public void activar() {
-		esActivo=true;		
+		esActivo = true;
 	}
-	
+
 	public boolean inicioRandom() {
 		Random random = new Random();
 		int randomInt = random.nextInt(2);
-		if(randomInt == 0) {
+		if (randomInt == 0) {
 			return false;
 		}
 		return true;
-	}	
-	
-	
+	}
+
 	public boolean getDireccion() {
 		return direccion;
 	}
-			
-	
-	
-	
-	public void moverIzquierda()
-	{
+
+	public void moverIzquierda() {
 		this.x = this.x - this.velocidad;
 	}
-	
-	public void moverDerecha()
-	{
+
+	public void moverDerecha() {
 		this.x = this.x + this.velocidad;
 	}
-	
-	
-	public void moverGnomo(Gnomo gnomo){
-				
-		if(gnomo.direccion) {
+
+	public void mover(Gnomo gnomo) {
+		if (gnomo.direccion) {
 			gnomo.moverDerecha();
-		}else {
+		} else {
 			gnomo.moverIzquierda();
 		}
-		
 	}
-	
-	public void dibujar(Entorno entorno)
-	{
+
+	public void dibujar(Entorno entorno) {
 		Image gnomo = Herramientas.cargarImagen("imagenes/gnomo.png");
 		entorno.dibujarImagen(gnomo, x, y, 0, 0.015);
 	}
-		
-	  
 
 	public int getX() {
 		return x;
@@ -95,33 +80,28 @@ public class Gnomo {
 	public int getY() {
 		return y;
 	}
-	
+
 	public int getRadio() {
 		return radio;
 	}
-	
+
 	public int getExtremoDer() {
-		return x+15;
+		return x + 15;
 	}
-	
+
 	public int getExtremoIzq() {
-		return x-15;
+		return x - 15;
 	}
-	
+
 	public int getYBase() {
-		return y+20; //modificar para la base
+		return y + 20; // modificar para la base
 	}
-	
+
 	public int getYAltura() {
-		return y-20;
+		return y - 20;
 	}
-	
+
 	public int getVelocidad() {
 		return velocidad;
 	}
-
-	
-	
-	
-	
 }
--- ProyectoLimpio/src/juego/Tortuga.java
+++ ProyectoLimpio/src/juego/Tortuga.java
@@ -2,120 +2,110 @@ package juego;
 
 import entorno.Entorno;
 import entorno.Herramientas;
-
 import java.awt.Image;
 import java.util.Random;
 
 public class Tortuga {
 	private int x;
-	  private int y;
-	  public int posInicial;
-	  static private int alto = 44;
-	  static private int ancho = 60;
-	  private int velocidad;
-	  private boolean cayendo = true;
-	  private Image turtleDer = Herramientas.cargarImagen("imagenes/turtle-der.png");
-	  private Image turtleIzq = Herramientas.cargarImagen("imagenes/turtle-izq.png");
-	  public boolean sobreBloque = false;
-	  public boolean activar=false;
-	  public int cantRebotes=0;
+	private int y;
+	public int posInicial;
+	static private int alto = 44;
+	static private int ancho = 60;
+	private int velocidad;
+	private boolean cayendo = true;
+	private Image turtleDer = Herramientas.cargarImagen("imagenes/turtle-der.png");
+	private Image turtleIzq = Herramientas.cargarImagen("imagenes/turtle-izq.png");
+	public boolean sobreBloque = false;
+	public int cantRebotes = 0;
 
-	  public Tortuga() {
-		posInicial=randomX();
-	    this.x = posInicial;
-	    this.y = 20;
-	    
-	    Random rand = new Random();
-	    boolean bool = rand.nextBoolean();
-	    int num = rand.nextInt(3) + 2;
-	    if (bool) {
-	      this.velocidad = num;
-	    } else {
-	      this.velocidad = -1 * num;
-	    }
-	  }	  
-	  
-	  public int getPosInicial() {
-		  return posInicial;
-	  }
-	  
-	  
-	  public int randomX() {
-		  int[] posicionX = new int[7];
-		  int posInicial = 100;
-		  
-		  Random r = new Random();
-		  
-		  for (int i=0; i<posicionX.length;i++) {
-			  posicionX[i]=posInicial;
-			  posInicial+=100;
-		  }
-		  
-		  return posicionX[r.nextInt(posicionX.length)];
-		  
-	  }
+	public Tortuga() {
+		posInicial = randomX();
+		this.x = posInicial;
+		this.y = 20;
+		Random rand = new Random();
+		boolean bool = rand.nextBoolean();
+		int num = rand.nextInt(3) + 2;
+		if (bool) {
+			this.velocidad = num;
+		} else {
+			this.velocidad = -1 * num;
+		}
+	}
 
-	  public void dibujar(Entorno entorno) {
-	    Image turtle = this.velocidad > 0 ? turtleDer : turtleIzq;
-	    entorno.dibujarImagen(turtle, x, y, 0, 0.1);	    
-	  }
+	public int getPosInicial() {
+		return posInicial;
+	}
 
-	  public int getX() {
-	    return x;
-	  }
+	public int randomX() {
+		int[] posicionX = new int[7];
+		int posInicial = 100;
+		Random r = new Random();
+		for (int i = 0; i < posicionX.length; i++) {
+			posicionX[i] = posInicial;
+			posInicial += 100;
+		}
+		return posicionX[r.nextInt(posicionX.length)];
+	}
 
-	  public int getY() {
-	    return y + (alto / 2);
-	  }
+	public void dibujar(Entorno entorno) {
+		Image turtle = this.velocidad > 0 ? turtleDer : turtleIzq;
+		entorno.dibujarImagen(turtle, x, y, 0, 0.1);
+	}
 
-	  static public int getAlto() {
-	    return alto;
-	  }
+	public int getX() {
+		return x;
+	}
 
-	  static public int getAncho() {
-	    return ancho;
-	  }
+	public int getY() {
+		return y + (alto / 2);
+	}
 
-	  public boolean getCayendo() {
-	    return cayendo;
-	  }
+	static public int getAlto() {
+		return alto;
+	}
 
-	  public int getVelocidad() {
-	    return velocidad;
-	  }
+	static public int getAncho() {
+		return ancho;
+	}
 
-	  public void setCayendo(boolean cae) {
-	    this.cayendo = cae;
-	  }
+	public boolean getCayendo() {
+		return cayendo;
+	}
 
-	  public void rebotar() {
-	    this.velocidad = this.velocidad * (-1);
-	    this.x = this.x + this.velocidad;
-	  }
+	public int getVelocidad() {
+		return velocidad;
+	}
 
-	  public void mover() {
-	    this.x = this.x + this.velocidad;
-	  }
+	public void setCayendo(boolean cae) {
+		this.cayendo = cae;
+	}
 
-	  public void caer() {
-	    this.y = this.y + 4;
-	  }
-	  
-	  public int getYBase() {
-		  return y+20;
-	  }
-	  
-	  public int getYAltura() {
-		  return y-20;
-	  }
-	  
-	  public int getExtremoDer() {
-			return x+25;
-	  }
-	  
-	  public int getExtremoIzq() {
-		  return x-25;
-	  }
-		
+	public void rebotar() {
+		this.velocidad = this.velocidad * (-1);
+		this.x = this.x + this.velocidad;
+	}
 
+	public void mover() {
+		this.x = this.x + this.velocidad;
+	}
+
+	public void caer() {
+		this.y = this.y + 4;
+	}
+
+	public int getYBase() {
+		return y + 12;
+	}
+
+	public int getYAltura() {
+		return y - 20;
+	}
+
+	public int getExtremoDer() {
+		return x + 25;
+	}
+
+	public int getExtremoIzq() {
+		return x - 25;
+	}
 }

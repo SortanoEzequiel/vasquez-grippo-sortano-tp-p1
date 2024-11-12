e6fcdccc14f4feb5ed9ce7df832c4fb5627318ff
EzequielSortano <sortano59@gmail.com>
2024-11-11 21:38:15 -0300

    Update Tortuga.java

--- ProyectoLimpio/src/juego/Tortuga.java
+++ ProyectoLimpio/src/juego/Tortuga.java
@@ -10,17 +10,15 @@ import java.util.Random;
 public class Tortuga {
 	private int x;
 	  private int y;
-	  static private int alto = 44;
-	  static private int ancho = 60;
+	  private int alto = 44;
+	  private int ancho = 60;
 	  private int velocidad;
 	  private boolean cayendo = true;
 	  private Image turtleDer = Herramientas.cargarImagen("imagenes/turtle-der.png");
 	  private Image turtleIzq = Herramientas.cargarImagen("imagenes/turtle-izq.png");
-	  public boolean sobreBloque = false;
-	  public boolean activar=false;
 
-	  public Tortuga() {
-	    this.x = randomX(posicionUtilizada);
+	  public Tortuga(int x) {
+	    this.x = x;
 	    this.y = 20;
 	    Random rand = new Random();
 	    boolean bool = rand.nextBoolean();
@@ -31,40 +29,10 @@ public class Tortuga {
 	      this.velocidad = -1 * num;
 	    }
 	  }
-	  
-	  public int[] posicionUtilizada = new int[7];
-	  
-	  public int randomX(int[] posicionUtilizada) {
-		  int[] posicion = new int[7];
-		  int posInicial = 100;
-		  
-		  Random r = new Random();
-		  
-		  for (int i=0; i<posicion.length;i++) {
-			  posicion[i]=posInicial;
-			  posInicial+=100;
-			  System.out.println(posicion[i]);
-		  }
-		  
-		  int posRandom = r.nextInt(posicion.length);
-
-		  for (int pos = 0; pos < posicionUtilizada.length; pos++) {
-			  
-		        posRandom = r.nextInt(posicion.length);
-
-		        // Si la posición no ha sido utilizada, la seleccionamos y la marcamos
-		        if (posicionUtilizada[pos]!=posicion[posRandom]) {
-		            posicionUtilizada[posRandom] = posicion[posRandom];  // Marcamos la posición como utilizada
-		            break;  // Salimos del bucle una vez que encontramos una posición válida
-		        }
-		  }
-		  return posicionUtilizada[posRandom];
-		  
-	  }
 
 	  public void dibujar(Entorno entorno) {
 	    Image turtle = this.velocidad > 0 ? turtleDer : turtleIzq;
-	    entorno.dibujarImagen(turtle, x, y, 0, 0.1);	    
+	    entorno.dibujarImagen(turtle, x, y, 0, 0.1);
 	  }
 
 	  public int getX() {
@@ -75,11 +43,11 @@ public class Tortuga {
 	    return y + (alto / 2);
 	  }
 
-	  static public int getAlto() {
+	  public int getAlto() {
 	    return alto;
 	  }
 
-	  static public int getAncho() {
+	  public int getAncho() {
 	    return ancho;
 	  }
 
@@ -105,7 +73,7 @@ public class Tortuga {
 	  }
 
 	  public void caer() {
-	    this.y = this.y + 3;
+	    this.y = this.y + 2;
 	  }
 	  
 	  public int getYBase() {
